5e0bcf9c4ac414535a02ff68ac2b0dd766c15d4f
pooja <poojaeb123@gmail.com>
2025-02-19 16:13:32 +0530

    adding the required files

---
+++ src/main/java/com/example/dao/DineInOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineInOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineInOrderApiApplication.class, args);
+	}
+
+}
